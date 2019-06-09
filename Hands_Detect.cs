685e6c2e970104f11e6165442467bfcc49e0d350
Jen-Tun Lee <bmpsst511@gmail.com>
2019-06-09 11:35:53 +0800

    Create Hands_Detect.cs

---
+++ Hands_Detect.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Leap;
+using Leap.Unity;
+
+public class Hands_Detect : MonoBehaviour {
+LeapProvider provider;
+
+void Start ()
+{
+    provider = FindObjectOfType<LeapProvider>() as LeapProvider;
+}
+
+void Update ()
+{
+    /****檢測是左手還是右手 *****/
+    /*Frame frame = provider.CurrentFrame;
+    foreach (Hand hand in frame.Hands)
+    {
+         if (hand.IsLeft) {
+            Debug.Log ("Left hand is present");
+        } else if (hand.IsRight) {
+            Debug.Log ("Right hand is present");
+        }
+        if(hand.IsLeft && hand.IsRight == true){
+            Debug.Log ("Both hand are present");
+        }
+    }*/
+
+    /****檢測是否雙手同時出現 *****/
+    Frame frame = provider.CurrentFrame; // controller is a Controller object
+    if(frame.Hands.Count > 1){
+    List<Hand> hands = frame.Hands;
+    Hand firstHand = hands [0];
+    Hand Second_tHand = hands [1];
+    Debug.Log ("both");
+    }
+}
+}
