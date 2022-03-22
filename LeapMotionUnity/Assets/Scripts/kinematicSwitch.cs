4138bc46ae15edac35574b68cf56b8845622473e
Jen-Tun Lee <bmpsst511@gmail.com>
2022-03-23 02:56:47 +0800

    scene update

--- LeapMotionUnity/Assets/Scripts/kinematicSwitch.cs
+++ LeapMotionUnity/Assets/Scripts/kinematicSwitch.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class kinematicSwitch : MonoBehaviour
 {
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    public void disableKin()
     {
-        
+        rb.isKinematic = false;
+    }
+    public void enableKin()
+    {
+        rb.isKinematic = true;
     }
 }
