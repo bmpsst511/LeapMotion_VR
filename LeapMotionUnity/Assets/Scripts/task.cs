94d367ccecbc94f3d34bd7a1121f8fd07108fbb7
bmpsst511 <bmpsst511@gmail.com>
2022-03-24 02:17:44 +0800

    cups position random appear

--- LeapMotionUnity/Assets/Scripts/task.cs
+++ LeapMotionUnity/Assets/Scripts/task.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class task : MonoBehaviour {
     public GameObject[] tasks;
+    public GameObject[] cups;
+    Vector3 [] cupPositions;
     int taskIndex;
     bool excuteOnce = false;
+    float x,y,z;
+
+    void Start(){
+        cupPositions = new Vector3[cups.Length];
+        for(int i = 0; i<9; i++){
+            cupPositions[i] = cups[i].transform.localPosition;
+            x = Random.Range(-0.2f, 0.2f);
+            y = Random.Range(-0.2f, 0.2f);
+            z = Random.Range(-0.2f, 0.2f);
+            cupPositions[i].x += x;
+            cupPositions[i].y += y;
+            cupPositions[i].z += z;
+            cups[i].transform.localPosition = cupPositions[i];
+        }
+        Debug.Log(y);
+    }
 
     public void assignTask() //this function call by StageRoot/Cube UI Panel/Cube UI Buttons/Cube UI Button(1)
     {
@@ -13,7 +31,7 @@ public class task : MonoBehaviour {
         {
             taskIndex = Random.Range(0, 3);
             Debug.Log(taskIndex);
-            tasks[taskIndex].SetActive(true);
+            tasks[taskIndex].SetActive(true);           
             excuteOnce = true;
         }
     }
